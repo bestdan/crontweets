1e2478abd638c0b245e70edcc99dad9120c4ea40
Daniel P. Egan <dp.egan@gmail.com>
2018-06-14 10:11:31 -0400

    fixing validateSchedule.

--- R/validateSchedule.R
+++ R/validateSchedule.R
@@ -14,8 +14,6 @@
 
 validateSchedule <- function(schedule){
   
-  dow_failures <- which(!schedule$dow %in% seq(0,6) & !is.na(schedule$dow))
-  
   valid_names <- c("minute", "hour", "dow", "tz", "category", "id")
   
   namecheck <- sort(names(schedule)) == sort(c("minute", "hour", "dow", "tz", "category", "id"))
@@ -24,18 +22,19 @@ validateSchedule <- function(schedule){
     stop("schedule must have column names ", paste0(valid_names, collapse = ", "))
   }
   
+  dow_failures <- which(!schedule$dow %in% seq(0,6) & !is.na(schedule$dow))
   if(length(dow_failures) > 0){
     stop(paste0("All dow entries must be one of integers 0 to 6. These rows fail: ", 
                 paste(dow_failures, collapse = ",")))
   }
   
-  hour_failures <- which(!schedule$hour %in% seq(0, 23) & !is.na(schedule$dow))
+  hour_failures <- which(!schedule$hour %in% seq(0, 23) & !is.na(schedule$hour))
   if(length(hour_failures) > 0){
     stop(paste0("All hour entries must be one of integers 0 to 23, These rows fail: ", 
                 paste(hour_failures, collapse = ",")))
   }
   
-  minute_failures <- which(!schedule$minute %in% seq(0, 59) & !is.na(schedule$dow))
+  minute_failures <- which(!schedule$minute %in% seq(0, 59) & !is.na(schedule$minute))
   if(length(minute_failures)  > 0){
     stop(paste0("All minute entries must be one of integers 0 to 23. These rows fail: ", 
                 paste(minute_failures, collapse = ",")))
