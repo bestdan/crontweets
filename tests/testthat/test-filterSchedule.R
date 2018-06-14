15a161acb0eea07150cbeaebf88e7f86307b3ad5
Daniel P. Egan <dp.egan@gmail.com>
2018-06-14 12:46:39 -0400

    more

--- tests/testthat/test-filterSchedule.R
+++ tests/testthat/test-filterSchedule.R
@@ -14,7 +14,7 @@ test_that("", {
 test_that("filterSchedule works with different time zones", {
   
   current_time <- as.POSIXlt("2018-06-13 6:00:55", tz = "EST")  # Current time defined in EST
-  lubridate::with_tz(current_time,tzone = "GMT" )
   now <- crontwit:::nowFormatted(current_time)
+  
   filterSchedule(schedule_item = schedule[7,], now = now, minute_range)  
 })
